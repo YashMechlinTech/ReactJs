4a516518f31deb8042b52f2c74f0fec47ee3e9d1
YashMechlinTech <Yash.chauhan@mechlintech.com>
2024-07-25 18:32:03 +0530

    DOM is learned

---
+++ js prequisites/4script.js
@@ -0,0 +1,16 @@
+//window object the window objec represents the open window in a browser .it is browser's object (not javascript's ) & is automatically created by the browser
+
+//it is a global object with lots of the properties and methods
+console.log("hello")
+window.console.log("helllo2")
+
+//console.log is actually an part of the window object .. 
+ //alert is also an window object part but we dont write the window keyword the browser already know it
+
+console.dir()//properties and method ko print kara deta hain
+
+console.log(window.document) //window giving error in the vs code but in browser it will run properly .  
+
+//dom is an tree structure of the html document and each component is called the node .
+
+// window->document->html ->Head,body, etc..
--- js prequisites/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-let x=90
-do{
-    console.log(x)
-x--
-}
-while(x!=0)
