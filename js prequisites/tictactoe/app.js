a90bcc04bd6299d7b2c8e7b404e44ee9388c04fb
YashMechlinTech <Yash.chauhan@mechlintech.com>
2024-07-27 00:08:28 +0530

    tictactoe

---
+++ js prequisites/tictactoe/app.js
@@ -0,0 +1,55 @@
+let boxes = document.querySelectorAll(".box");
+
+let resetbutton = document.querySelector("#reset-btn");
+
+let turnO = true; //player x and player y
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 4, 6],
+  [2, 5, 8],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+const checkWinner = () => {
+  for (let patterns of winPatterns) {
+    let pos1 = boxes[patterns[0]].innerText;
+    let pos2 = boxes[patterns[1]].innerText;
+    let pos3 = boxes[patterns[2]].innerText;
+
+    if (pos1 != "" && pos2 != "" && pos3 != "") {
+      if (pos1 == pos2 && pos2 == pos3) {
+        if (pos1 == "X") {
+          setTimeout("alert('player X is winner ');", 1);
+          return;
+        } else {
+          setTimeout("alert('player O is winner ');", 1); //
+          return;
+        }
+      }
+    }
+  }
+};
+
+resetbutton.addEventListener("click", () => {
+  location.reload();
+});
